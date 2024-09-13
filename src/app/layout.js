124ab6ac2ef786d1eaded348212d1ccff2ab2fcc
kasra <kasramajdy81@gmail.com>
2024-09-13 15:29:09 +0330

    fix project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,14 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../public/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../public/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Footer/>
       </body>
     </html>
   );
